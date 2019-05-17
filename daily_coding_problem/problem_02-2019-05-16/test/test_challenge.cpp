0bc7891dac8f06e0102d5b9ab68858b3373f5ac2
Peter Wolf <pwolf2310@gmail.com>
2019-05-17 09:55:24 +0200

    add template version for problem_02

---
+++ daily_coding_problem/problem_02-2019-05-16/test/test_challenge.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "challenge/challenge.h"
+}
+
+class challenge_test : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+};
+
+TEST_F(challenge_test, pos_test_01)
+{
+    bool ret = true;
+    ASSERT_TRUE(ret);
+}
