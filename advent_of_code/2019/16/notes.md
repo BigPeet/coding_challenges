800057bb789ae4581bb4378156af2152086ed9e1
Peter Wolf <pwolf2310@gmail.com>
2019-12-17 20:05:11 +0100

    added intial stuff for Day 16 of AoC 2019

---
+++ advent_of_code/2019/16/notes.md
@@ -0,0 +1,3 @@
+FFT is probably a hint towards Fast Fourier transforms.
+
+I will start with the "naive" approach, but I expect when trying to crack the input, this will run into timing issues.
---
+++ advent_of_code/2019/16/test/test_main.cpp
@@ -0,0 +1,13 @@
+/*
+ *
+ *  Author: Peter Wolf <pwolf2310@gmail.com>
+ *
+ */
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
