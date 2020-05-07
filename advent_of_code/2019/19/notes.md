ae46b03b038d8c4a60bae05233a42ac672e24776
Peter Wolf <pwolf2310@gmail.com>
2020-05-07 21:29:25 +0200

    add solution for part 2 of day19

--- advent_of_code/2019/19/notes.md
+++ advent_of_code/2019/19/notes.md
@@ -6,3 +6,21 @@ Repeat.
 You don't necessarily need to save the results to get the solution (yet).
 
 The solution to the first part is 231.
+
+For part two I have the following considerations:
+
+* To fit 100x100 square (aligned to the grid), needs to be at least 100 points wide, but since it is angled, it needs up to 200 (depending on the exact angle).
+* You can move along the beam when moving roughly along the (1,1) vector, but for greater distances a more accurate slope needs to be calculated.
+    * This can be achieved by calculating the mid-point of the beam for each row and the fitting them into a line.
+* The beam width increases roughly every 3 rows.
+
+These considerations should help to find the approximate region for the 100x100 square.
+Then you can check row-wise if a square would fit by checking its corner coordinates.
+I think every possible candidate square's top-right corner would be positioned in the right-most field of the beam.
+Alternatively checking the bottom right corner could eliminate a lot of checking.
+
+After first calculations the slope of the beam seems to be ~1.22.
+
+The solution for part two is 9210745.
+
+The code could certainly be optimized by limiting the search space even more, e.g. calculating which beam width is needed for the square to fit and start your search there, but this simple approach works.
--- advent_of_code/2019/19/src/main.c
+++ advent_of_code/2019/19/src/main.c
@@ -12,11 +12,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-/*scan area */
-#define HEIGHT 50
-#define WIDTH 50
-
-
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -34,19 +29,108 @@ int main(int argc, char* argv[])
         return 0;
     }
 
+    /*Part 01*/
     int sum = 0;
-
-    for (int y = 0; y < HEIGHT; ++y)
+    for (int y = 0; y < 50; ++y)
     {
-        for (int x = 0; x < WIDTH; ++x)
+        for (int x = 0; x < 50; ++x)
         {
             int64_t result = scan_coordinate(prog, x, y);
             /*printf("Scan result for (%d, %d): %ld\n", x, y, result);*/
             sum += result;
         }
     }
-
     printf("Total number of affected points: %d\n", sum);
+
+    /*Part 02*/
+
+    /*Calculate slope of the beam.*/
+    int row   = 300;
+    int cols  = 500;
+    int beam  = 0;
+    int first = -1;
+    int last  = -1;
+    for (int x = 0; x < cols; ++x)
+    {
+        int64_t result = scan_coordinate(prog, x, row);
+        if (beam ^ result)
+        {
+            if (!beam)
+            {
+                first = x;
+            }
+            else
+            {
+                last = x - 1;
+            }
+            beam = !beam;
+        }
+    }
+    if (beam)
+    {
+        printf("Didn't find end of beam, not enough columns.\n");
+    }
+    float slope = ((first + last) / 2.0) / (row + 1);
+    printf("Slope for row #%d is %f.\n", row, slope);
+
+    /*Since the width of the beam increases about every 3rd row,
+     * the beam will hit a width of 100 at roughly this offset.
+    * Looking at earlier rows is not really useful.*/
+    row = 300;
+
+    /*The beam should be between 100-200 fields wide in the area we are looking
+     * for.*/
+    cols = 250;
+
+    int square_size = 100;
+
+    int found = 0;
+    while (!found)
+    {
+        /*We are only interested in the beam area, so we offset the column*/
+        /*based on our estimate of the beam center.*/
+        int col_offset = (int) (row * slope);
+        beam           = 1;
+
+        /*Find the right most field affected by the beam.*/
+        /*We start looking roughly in the center of the beam.*/
+        for (int x = 0; x < cols; ++x)
+        {
+            int64_t result = scan_coordinate(prog, x + col_offset, row);
+            if (beam ^ result)
+            {
+                if (beam)
+                {
+                    /*End of beam found.*/
+                    /*last field of beam is in x-1.*/
+
+                    int right_x  = x - 1 + col_offset;
+                    int top_y    = row;
+                    int left_x   = right_x - (square_size - 1);
+                    int bottom_y = top_y + (square_size - 1);
+
+                    if (left_x >= 0)
+                    {
+                        /*check the corner coordinates.*/
+                        int64_t top_left = scan_coordinate(prog, left_x, top_y);
+                        int64_t bottom_left =
+                            scan_coordinate(prog, left_x, bottom_y);
+                        int64_t bottom_right =
+                            scan_coordinate(prog, right_x, bottom_y);
+
+                        if (top_left && bottom_left && bottom_right)
+                        {
+                            printf("Top Left: (%d, %d)\n", left_x, top_y);
+                            found = 1;
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+        row++;
+    }
 
     destroy_intcode(prog);
     return 0;
