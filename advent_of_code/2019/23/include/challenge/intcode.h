1b80ac3ff057395abc0d04b7317b0ab93ed50635
Peter Wolf <pwolf2310@gmail.com>
2021-10-29 17:19:42 +0200

    applying fixits to intcode header

--- advent_of_code/2019/23/include/challenge/intcode.h
+++ advent_of_code/2019/23/include/challenge/intcode.h
@@ -47,37 +47,37 @@ typedef struct
     int waiting_for_input;
 } intcode_t;
 
-intcode_t* read_intcode(const char* const file_path);
-intcode_t* create_intcode(int64_t* const content, const size_t content_size);
-void destroy_intcode(intcode_t* const prog);
-void print_intcode(const intcode_t* const prog);
-int set_mem_value(intcode_t* const prog, const size_t address, const int64_t value);
-int64_t get_mem_value(const intcode_t* const prog, const size_t address);
-void set_io_mode(intcode_t* const prog, const intcode_io_mode_t mode);
-void set_mem_io_in(intcode_t* const prog, intcode_io_mem_t* const input_store);
-void set_mem_io_out(intcode_t* const prog, intcode_io_mem_t* const output_store);
-void set_std_io_in(intcode_t* const prog, FILE* const input_stream);
-void set_std_io_out(intcode_t* const prog, FILE* const output_stream);
-intcode_t* copy_intcode(const intcode_t* const prog);
-int output_intcode(const intcode_t* const prog);
-int waiting_for_input(const intcode_t* const prog);
+intcode_t* read_intcode(const char* file_path);
+intcode_t* create_intcode(int64_t* memory, size_t content_size);
+void destroy_intcode(intcode_t* prog);
+void print_intcode(const intcode_t* prog);
+int set_mem_value(intcode_t* prog, size_t address, int64_t value);
+int64_t get_mem_value(const intcode_t* prog, size_t address);
+void set_io_mode(intcode_t* prog, intcode_io_mode_t mode);
+void set_mem_io_in(intcode_t* prog, intcode_io_mem_t* input_store);
+void set_mem_io_out(intcode_t* prog, intcode_io_mem_t* output_store);
+void set_std_io_in(intcode_t* prog, FILE* input_stream);
+void set_std_io_out(intcode_t* prog, FILE* output_stream);
+intcode_t* copy_intcode(const intcode_t* prog);
+int output_intcode(const intcode_t* prog);
+int waiting_for_input(const intcode_t* prog);
 
 intcode_io_mem_t* create_io_mem();
-void destroy_io_mem(intcode_io_mem_t* const store);
+void destroy_io_mem(intcode_io_mem_t* store);
 
-int execute(intcode_t* const prog);
-int execute_head_block(intcode_t* const prog, int* const op_code);
+int execute(intcode_t* prog);
+int execute_head_block(intcode_t* prog, int* op_code);
 
-int add_op(intcode_t* const prog, const int64_t* const parameters);
-int multiply_op(intcode_t* const prog, const int64_t* const parameters);
-int input_op(intcode_t* const prog, const int64_t* const parameters);
-int output_op(intcode_t* const prog, const int64_t* const parameters);
-int jmp_if_true_op(intcode_t* const prog, const int64_t* const parameters);
-int jmp_if_false_op(intcode_t* const prog, const int64_t* const parameters);
-int is_less_op(intcode_t* const prog, const int64_t* const parameters);
-int is_equals_op(intcode_t* const prog, const int64_t* const parameters);
-int adjust_rel_base_op(intcode_t* const prog, const int64_t* const parameters);
-int error_op(intcode_t* const prog, const int64_t* const parameters);
+int add_op(intcode_t* prog, const int64_t* parameters);
+int multiply_op(intcode_t* prog, const int64_t* parameters);
+int input_op(intcode_t* prog, const int64_t* parameters);
+int output_op(intcode_t* prog, const int64_t* parameters);
+int jmp_if_true_op(intcode_t* prog, const int64_t* parameters);
+int jmp_if_false_op(intcode_t* prog, const int64_t* parameters);
+int is_less_op(intcode_t* prog, const int64_t* parameters);
+int is_equals_op(intcode_t* prog, const int64_t* parameters);
+int adjust_rel_base_op(intcode_t* prog, const int64_t* parameters);
+int error_op(intcode_t* prog, const int64_t* parameters);
 
 
 #endif /* ifndef INCLUDE_CHALLENGE_LIB_H */
