bb85b838d7857f0eb830dcb022bddfc7e42d1afb
Peter Wolf <pwolf2310@gmail.com>
2021-11-05 01:57:49 +0100

    Solved Part 2 of Day 1 of AoC 2020

--- advent_of_code/2020/day01/notes.md
+++ advent_of_code/2020/day01/notes.md
@@ -11,3 +11,15 @@ If their sum is under 2020, replace the smallest number with the 2nd smallest nu
 I'm not sure, if that works, but if it does, then it should run in O(n) (well, O(nlog(n)), due to the sorting).
 
 Solution for part 1: 902451
+
+Well, as I've guessed the second part is a little more complex.
+The naive approach would result in O(n^3).
+
+While my chosen approach for part 1 can not be easily extended to 3 numbers, I can implement a "hybrid" approach:
+
+Select the lowest value as fixed value x.
+Then use the previous method to check if in the remaining range there are two values a, b for which a + b == 2020 - x.
+If not, then increase x to the next highest value.
+This should be in O(n^2), if I'm not mistaken.
+
+Solution for part 2: 85555470
--- advent_of_code/2020/day01/src/main.rs
+++ advent_of_code/2020/day01/src/main.rs
@@ -20,7 +20,7 @@ fn parse_args() -> Result<String, InputError> {
     if args.len() != 2 {
         return Err(InputError::WrongNumberOfArgs);
     }
-    // TODO: is there a better way? Does this even avoid an allocation?
+    // FIXME: is there a better way? Does this even avoid an allocation?
     if let Some(file_path) = args.into_iter().nth(1) {
         Ok(file_path)
     } else {
@@ -32,7 +32,7 @@ fn parse_input(path: String) -> Result<Vec<i32>, InputError> {
     let f = File::open(path)?;
     let f = BufReader::new(f);
 
-    //TODO: Add error handling instead of silently dropping errors
+    // FIXME: Add error handling instead of silently dropping errors
     let mut numbers: Vec<i32> = f
         .lines()
         .into_iter()
@@ -43,33 +43,59 @@ fn parse_input(path: String) -> Result<Vec<i32>, InputError> {
     Ok(numbers)
 }
 
-// Assumes the passed in vector is sorted!
-fn find_summands(vec: &Vec<i32>, value: i32) -> Option<(i32, i32)> {
-    let mut high_idx = vec.len() - 1;
-    let mut low_idx = 0;
-
+fn find_two(
+    vec: &Vec<i32>,
+    mut high_idx: usize,
+    mut low_idx: usize,
+    target: i32,
+) -> Option<(i32, i32)> {
     while low_idx < high_idx {
         let high = vec[high_idx];
         let low = vec[low_idx];
         let sum = high + low;
-        if sum == value {
+        if sum == target {
             return Some((low, high));
-        } else if sum > value {
+        } else if sum > target {
             high_idx -= 1;
         } else {
             low_idx += 1;
         }
     }
+    None
+}
 
+// Assumes the passed in vector is sorted!
+fn find_summands_p1(vec: &Vec<i32>, value: i32) -> Option<(i32, i32)> {
+    find_two(vec, vec.len() - 1, 0, value)
+}
+
+// Assumes the passed in vector is sorted!
+fn find_summands_p2(vec: &Vec<i32>, value: i32) -> Option<(i32, i32, i32)> {
+    let mut fix_idx = 0;
+    while fix_idx < vec.len() - 2 {
+        let fix = vec[fix_idx];
+        if let Some((a, b)) = find_two(vec, vec.len() - 1, fix_idx + 1, value - fix) {
+            return Some((a, b, fix));
+        }
+        fix_idx += 1;
+    }
     None
 }
 
 fn main() -> Result<(), InputError> {
     let numbers = parse_args().and_then(|path| parse_input(path))?;
 
-    if let Some((a, b)) = find_summands(&numbers, 2020) {
+    println!("Part 1:");
+    if let Some((a, b)) = find_summands_p1(&numbers, 2020) {
         println!("Found entries: {} + {} = {}", a, b, 2020);
         println!("Multiplication: {} * {} = {}", a, b, a * b);
+    } else {
+        println!("No match found!");
+    }
+    println!("Part 2:");
+    if let Some((a, b, c)) = find_summands_p2(&numbers, 2020) {
+        println!("Found entries: {} + {} + {} = {}", a, b, c, 2020);
+        println!("Multiplication: {} * {} * {} = {}", a, b, c, a * b * c);
     } else {
         println!("No match found!");
     }
