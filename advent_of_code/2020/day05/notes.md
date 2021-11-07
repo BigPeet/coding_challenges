1ea3caa61111b4e228d9d9ce17d6bdf1d5e98ab4
Peter Wolf <pwolf2310@gmail.com>
2021-11-07 01:39:12 +0100

    Solved part 2 of Day 5 of AoC 2020

--- advent_of_code/2020/day05/notes.md
+++ advent_of_code/2020/day05/notes.md
@@ -7,3 +7,12 @@ FBFBBFFRLR becomes two binary numbers:
 0101100 (= 44) and 101 (= 5).
 
 Solution for the first part: 913
+
+For the second part the naive solution is:
+
+Iterate over all possible seats (or IDs), check if the ID is in the parsed list. If not, check if the +/-1 IDs are in the list.
+If searching is done in O(n), the this would be in O(n^2).
+
+Using a data structure with O(1) search/access could be an improvement, lowering it to O(n) overall.
+
+Solution for the second part: 717
--- advent_of_code/2020/day05/src/main.rs
+++ advent_of_code/2020/day05/src/main.rs
@@ -2,6 +2,8 @@ use day05::Seat;
 use parsing::InputError;
 use std::env;
 
+const NUM_OF_SEATS: usize = 128 * 8;
+
 fn main() -> Result<(), InputError> {
     let seats: Vec<Seat> = parsing::filepath_from_args(env::args().collect())
         .and_then(parsing::get_lines)
@@ -9,8 +11,20 @@ fn main() -> Result<(), InputError> {
 
     println!(
         "Part 1: The highest ID is {}.",
-        seats.iter().map(|s| s.id()).max().unwrap_or(0)
+        seats.iter().map(Seat::id).max().unwrap_or(0)
     );
+
+    // Allows for contant-time check if a seat is in the parsed list.
+    let mut taken = [false; NUM_OF_SEATS];
+    seats.iter().for_each(|s| taken[s.id()] = true);
+
+    // Skip first and last id, can't be it.
+    for id in 1..(NUM_OF_SEATS - 1) {
+        if !taken[id] && taken[id - 1] && taken[id + 1] {
+            println!("Part 2: Your seath ID is {}", id);
+            return Ok(());
+        }
+    }
 
     Ok(())
 }
