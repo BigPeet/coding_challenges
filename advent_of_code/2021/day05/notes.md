ed411660334e1e69b23e54680b891d915b2d41f0
Peter Wolf <pwolf2310@gmail.com>
2021-12-07 22:20:31 +0100

    Solved Part 2 of Day 05 of AoC 2021

--- advent_of_code/2021/day05/notes.md
+++ advent_of_code/2021/day05/notes.md
@@ -1,1 +1,5 @@
 The solution for the first part is 6225.
+
+The second part basically requires me to simply remove the diagonal-filter.
+
+The solution for the second part is 22116.
--- advent_of_code/2021/day05/src/main.rs
+++ advent_of_code/2021/day05/src/main.rs
@@ -19,5 +19,17 @@ fn main() -> ParsingResult {
         point_map.values().filter(|v| **v > 1).count()
     );
 
+    // Part 2
+    let mut point_map: HashMap<Point, usize> = HashMap::new();
+    for l in lines.iter() {
+        for p in l.points() {
+            point_map.entry(p).and_modify(|e| *e += 1).or_insert(1);
+        }
+    }
+    println!(
+        "Part 2: There are {} points where at least 2 lines overlap.",
+        point_map.values().filter(|v| **v > 1).count()
+    );
+
     Ok(())
 }
