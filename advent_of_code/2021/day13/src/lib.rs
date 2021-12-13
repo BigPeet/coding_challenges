7b2e4fe01d40fe26e958ae093cc4f187f2f0ff7c
Peter Wolf <pwolf2310@gmail.com>
2021-12-13 18:04:22 +0100

    Solved Part 1 of Day 13 of AoC 2021

---
+++ advent_of_code/2021/day13/src/lib.rs
@@ -0,0 +1,88 @@
+use parsing::InputError;
+use std::collections::HashSet;
+use std::str::FromStr;
+
+type SparseMatrix = HashSet<Point>;
+
+pub struct TransparentPaper {
+    data: SparseMatrix,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+
+pub enum FoldInstruction {
+    Horizontal(usize),
+    Vertical(usize),
+}
+
+impl TransparentPaper {
+    pub fn new(points: Vec<Point>) -> TransparentPaper {
+        TransparentPaper {
+            data: SparseMatrix::from_iter(points.into_iter()),
+        }
+    }
+
+    pub fn apply(self, instruction: &FoldInstruction) -> TransparentPaper {
+        // FIXME: a little wasteful because we create a new hashset every time.
+        let mut new_data = SparseMatrix::new();
+        for mut point in self.data.into_iter() {
+            match *instruction {
+                FoldInstruction::Horizontal(row) => {
+                    if point.y > row {
+                        point.y = row - (point.y - row);
+                    }
+                }
+                FoldInstruction::Vertical(col) => {
+                    if point.x > col {
+                        point.x = col - (point.x - col);
+                    }
+                }
+            }
+            new_data.insert(point);
+        }
+        TransparentPaper { data: new_data }
+    }
+
+    pub fn visible_dots(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl FromStr for Point {
+    type Err = InputError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some((l, r)) = s.trim_end().split_once(',') {
+            Ok(Point {
+                x: l.parse::<usize>()?,
+                y: r.parse::<usize>()?,
+            })
+        } else {
+            Err(InputError::ParseGeneral)
+        }
+    }
+}
+
+impl FromStr for FoldInstruction {
+    type Err = InputError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let assign = s.trim_start_matches("fold along ");
+        if let Some((l, r)) = assign.trim().split_once('=') {
+            let value = r.parse::<usize>()?;
+            if l == "y" {
+                Ok(FoldInstruction::Horizontal(value))
+            } else if l == "x" {
+                Ok(FoldInstruction::Vertical(value))
+            } else {
+                Err(InputError::ParseGeneral)
+            }
+        } else {
+            Err(InputError::ParseGeneral)
+        }
+    }
+}
---
+++ advent_of_code/2021/day13/src/main.rs
@@ -0,0 +1,34 @@
+use day13::{FoldInstruction, Point, TransparentPaper};
+use parsing::InputError;
+use parsing::ParsingResult;
+
+fn main() -> ParsingResult {
+    let groups = parsing::filepath_from_args(std::env::args().collect())
+        .and_then(parsing::get_lines)
+        .and_then(parsing::get_line_groups)?;
+
+    if groups.len() != 2 {
+        return Err(InputError::ParseGeneral);
+    }
+
+    let points = groups[0]
+        .iter()
+        .map(|l| l.parse::<Point>())
+        .collect::<Result<Vec<Point>, InputError>>()?;
+    let instructions = groups[1]
+        .iter()
+        .map(|l| l.parse::<FoldInstruction>())
+        .collect::<Result<Vec<FoldInstruction>, InputError>>()?;
+
+    // Part 1
+    let mut paper = TransparentPaper::new(points);
+    for instruction in instructions.iter().take(1) {
+        paper = paper.apply(instruction);
+    }
+    println!(
+        "Part 1: After applying the first fold, {} dots are visible.",
+        paper.visible_dots()
+    );
+
+    Ok(())
+}
